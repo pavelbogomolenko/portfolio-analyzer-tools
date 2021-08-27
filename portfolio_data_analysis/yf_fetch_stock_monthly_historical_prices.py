6a0c65a70d7645cb8ea5ca50480fd1cda01ddd09
Pavlo Bogomolenko <pavlo.bogomolenko@gmail.com>
2021-08-27 10:58:17 +0200

    Add user agent headers and some logging

--- portfolio_data_analysis/yf_fetch_stock_monthly_historical_prices.py
+++ portfolio_data_analysis/yf_fetch_stock_monthly_historical_prices.py
@@ -7,18 +7,20 @@ import requests
 
 from historical_data.symbols import MARKET_INDICIES
 
+USER_AGENT_HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 DIRNAME = os.path.dirname(os.path.abspath(__file__))
-BASE_URL = "https://query1.finance.yahoo.com/v7/finance/download/" \
-           + "{}?period1=915145200&period2={}&interval=1d&events=history&includeAdjustedClose=true"
+BASE_URL = 'https://query1.finance.yahoo.com/v7/finance/download/' \
+           + '{}?period1=915145200&period2={}&interval=1d&events=history&includeAdjustedClose=true'
 
 
 def fetch_raw_data_for_symbol(symbol):
     now_ts = round(dt.today().timestamp())
     url = BASE_URL.format(symbol, now_ts)
-    print("Fetching data for: {}".format(symbol))
-    r = requests.get(url)
+    print("Fetching data for: {}".format(url))
+    r = requests.get(url, headers=USER_AGENT_HEADERS)
     if r.status_code != 200:
         print("API status_code was not 200. Will try again later")
+        print(r.text)
         return None
 
     return r.text
